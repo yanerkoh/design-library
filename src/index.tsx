b6c463f058a4d18c695b71c8cfa939572355a15b
yaner <e0970478@u.nus.edu>
2025-11-27 15:53:18 +0800

    fix tgz

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 export * from 'tamagui'
 export * from '@tamagui/toast'
-export * from './MyComponent'
-export * from './CustomToast'
-export * from './SwitchThemeButton'
-export * from './SwitchRouterButton'
-export * from './FormGroup'
-export * from './StepIndicator'
 
-// explicitly re-export Button and its props under unique names to avoid collisions with tamagui
+export * from './CustomToast'
+export * from './FormGroup'
+export * from './MyComponent'
+export * from './StepIndicator'
+export * from './SwitchRouterButton'
+export * from './SwitchThemeButton'
 export { Button as CustomButton } from './Button'
-export type { ButtonProps as CustomButtonProps } from './Button'
